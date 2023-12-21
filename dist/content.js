ab5ba56f6fa7158ece35988e6313f1bdd422df71
janardhan5031 <bjanardhan5031@gmail.com>
2023-12-21 08:36:41 +0530

    tab added to whatsapp

--- dist/content.js
+++ dist/content.js
@@ -1,32 +1,42 @@
 // public/content.js
 
-var app = document.createElement("div");
-app.id = "whatsapp-extension-root";
+var app = document.createElement('div');
+app.id = 'whatsapp-extension-root';
 document.body.appendChild(app);
 
-console.log("Container created");
+console.log('Container created');
 
 // Restricting the Whatsapp root element height
-const whatsappRoot = document.getElementById("app");
+const whatsappRoot = document.getElementById('app');
 if (whatsappRoot) {
-  whatsappRoot.style.position = "absolute";
-  whatsappRoot.style.top = "3.8rem";
-  whatsappRoot.style.height = "calc(100% - 3.8rem)";
+  whatsappRoot.style.position = 'absolute';
+  whatsappRoot.style.top = '3.8rem';
+  whatsappRoot.style.height = 'calc(100% - 3.8rem)';
 }
 
 // Loading js file
-var script = document.createElement("script");
-const jsFile = "./static/main.js";
+var script = document.createElement('script');
+const jsFile = './static/main.js';
 
 script.defer = true;
 script.src = chrome.runtime.getURL(jsFile);
 document.body.appendChild(script);
-console.log("Main.js fiel injected");
+console.log('Main.js fiel injected');
 
 // Loading css file
-var cssLink = document.createElement("link");
-const cssFile = "./static/style.css";
+var cssLink = document.createElement('link');
+console.log(cssLink);
+const cssFile = 'static/style.css';
 
 cssLink.href = chrome.runtime.getURL(cssFile);
 document.head.appendChild(cssLink);
-console.log("Css file injected");
+console.log('Css file injected');
+
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href =
+  'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css'; // Adjust the version as needed
+
+// Append the link element to the document's head
+document.head.appendChild(link);
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 import './App.css'
+import toast, { Toaster } from 'react-hot-toast';
 
-// const tabs:string[] = ["Tab1", "Tab2", "Tab3", "Tab4"]
+const tabs:string[] = ["Tab1", "Tab2", "Tab3", "Tab4"]
 
 function App() {
 
+  const Onclick = (tabName:string)=>{
+    console.log('Clicked')
+    toast.success(tabName);
+  }
+
   return (
-    <div style={{height:'3.8rem',width:"100%",backgroundColor:"#e6f0f0",position:"absolute",top:0,zIndex:100 }}>
-      <div className='flex flex-row'></div>
-        <div className='w-[4rem]' >Tab1</div>
-        <div className='w-[4rem]' >Tab2</div>
-        <div className='w-[4rem]' >Tab3</div>
-      <div>
-        Tab1
+    <div className="relative w-full px-8 fixed top-0 z-100 flex items-center" style={{backgroundColor:"#e6f0f0", height:"3.8rem"}}>
+      <div className="flex flex-row gap-4 ">
+        {
+          tabs.map((tab)=>(
+            <div className="w-20"><button 
+                style={{color:"black"}} 
+                className='hover:color-red-500' 
+                onClick={()=>{
+                  console.log('Clicked')
+                  toast.success(tab)
+                }}
+              >{tab}
+              </button>
+            </div>
+          ))
+        }
       </div>
+      <Toaster position="bottom-center" gutter={56} />
     </div>
   )
 }
